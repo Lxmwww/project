58817e38e75178bd2a102fbf3efcb4907c9f18c6
lxm <lxm@qq.com>
2022-03-16 10:55:05 +0800

    初始

---
+++ project-product/src/test/java/com/lxm/projectproduct/ProjectProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxm.projectproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ project-ware/src/main/java/com/lxm/projectware/ProjectWareApplication.java
@@ -0,0 +1,13 @@
+package com.lxm.projectware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectWareApplication.class, args);
+    }
+
+}
